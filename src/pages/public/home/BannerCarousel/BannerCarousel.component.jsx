569d05d05e8609f34b1f8251259123b65f0c723f
BinhDo <dt.binh@live.com>
2023-04-01 18:29:30 +0700

    Setup Banner Carousel

---
+++ src/pages/public/home/BannerCarousel/BannerCarousel.component.jsx
@@ -0,0 +1,86 @@
+import React, { useMemo, useRef } from 'react';
+import styles from './BannerCarousel.module.scss';
+import Slider from 'react-slick';
+import { SubBannerCarousel } from './SubBannerCarousel';
+import { useObserverItem } from 'components/hook/useObserverItem';
+
+
+function ArrowLef(props) {
+  const { currentSlide, onClick } = props;
+  return (
+    <div
+      className={
+        styles[`slider-arrow__left${currentSlide === 0 ? '--disabled' : ''}`]
+      }
+      onClick={onClick}
+    ></div>
+  );
+}
+
+function ArrowRight(props) {
+  const { currentSlide, slideCount, onClick } = props;
+  return (
+    <div
+      className={
+        styles[
+          `slider-arrow__right${
+            currentSlide > slideCount - 2 ? '--disabled' : ''
+          }`
+        ]
+      }
+      onClick={onClick}
+    ></div>
+  );
+}
+
+const BannerCarousel = ({ news }) => {
+  const refSlider = useRef(null);
+  // const refContent = useRef(null);
+
+  const settings = useMemo(
+    () => ({
+      className: styles['setting-slider'],
+      dotsClass: styles['setting-dots'],
+      variableWidth: true,
+      dots: true,
+      arrows: true,
+      swipeToSlide: true,
+      touchMove: true,
+      swipe: true,
+      infinite: false,
+      speed: 300,
+      slidesToShow: 1,
+      touchThreshold: 50,
+      slidesToScroll: 1,
+      adaptiveHeight: true,
+      lazyLoad: 'progressive',
+      cssEase: 'ease-out',
+      nextArrow: <ArrowRight />,
+      prevArrow: <ArrowLef />,
+    }),
+    []
+  );
+
+  useObserverItem(refSlider, styles);
+  // useChooseLanguage(testimonial, refContent);
+
+  return (
+    <>
+      <div className={styles['container']}>
+        <div className={styles['slider-wrapper']} ref={refSlider}>
+          <Slider {...settings}>
+            {Array.isArray(news) &&
+              news.map((item, index) => {
+                return item?.fields?.hightLight ? (
+                  <SubBannerCarousel key={index} news={item} />
+                ) : (
+                  ''
+                );
+              })}
+          </Slider>
+        </div>
+      </div>
+    </>
+  );
+};
+export default React.memo(BannerCarousel);
---
+++ src/pages/public/home/BannerCarousel/SubBannerCarousel/SubBannerCarousel.component.jsx
@@ -0,0 +1,114 @@
+import React, { useRef, useCallback } from 'react';
+import styles from './SubBannerCarousel.module.scss';
+import Image from 'next/image';
+import { useObserverItem } from 'components/hook/useObserverItem';
+import { get } from 'lodash';
+import { RoutePages } from '@constants/router';
+import { useRouter } from 'next/router';
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
+import { convertTime } from '@utils/uti';
+
+const SubBannerCarousel = ({ news }) => {
+  const { title, image, desc, slug, time, author, avatar } = news?.fields;
+  const router = useRouter();
+  const { locale } = useRouter();
+
+  const refContainer = useRef();
+
+  useObserverItem(refContainer, styles);
+
+  const redirectToPage = useCallback(
+    (_link) => {
+      router.push(_link);
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+    },
+    [router]
+  );
+
+  let _avatar = get(avatar, ['fields', 'file', 'url']);
+  let _imageURL = get(image, ['fields', 'file', 'url']);
+  let _imageWidth = get(image, ['fields', 'file', 'details', 'image', 'width']);
+  let _imageHeight = get(image, [
+    'fields',
+    'file',
+    'details',
+    'image',
+    'height',
+  ]);
+  let _imageAvatarrWidth = get(avatar, [
+    'fields',
+    'file',
+    'details',
+    'image',
+    'width',
+  ]);
+  let _imageAvatarrHeight = get(avatar, [
+    'fields',
+    'file',
+    'details',
+    'image',
+    'height',
+  ]);
+
+  const activeImage = (e) => {
+    let threshold = 3;
+    let distance = 0;
+
+    const onMouseMove = () => {
+      distance++;
+    };
+    const onMouseUp = () => {
+      if (distance <= threshold) redirectToPage(RoutePages.MARKET + `/${slug}`);
+    };
+    e.currentTarget.addEventListener('mousemove', onMouseMove);
+    e.currentTarget.addEventListener('mouseup', onMouseUp, { once: true });
+  };
+
+  return (
+    <>
+      <div
+        className={styles['container']}
+        ref={refContainer}
+        onMouseDown={(e) => activeImage(e)}
+      >
+        <div className={styles['main-image']}>
+          <Image
+            src={`https:${_imageURL}`}
+            alt=''
+            width={_imageWidth ? _imageWidth : 383}
+            height={_imageHeight ? _imageHeight : 200}
+            layout='fill'
+            objectFit='fill'
+            quality={100}
+          />
+        </div>
+        <div className={styles['client-wrapper']}>
+          <div className={styles['client']}>
+            <div className={styles['client__title']}>{title}</div>
+            <div className={styles['client__time']}>
+              {convertTime(time, locale)}
+            </div>
+            <div className={styles['client__desc']}>
+              {documentToReactComponents(desc)}
+            </div>
+            <div className={styles['author']}>
+              <div className={styles['author__avatar']}>
+                <Image
+                  src={`https:${_avatar}`}
+                  alt=''
+                  width={_imageAvatarrWidth ? _imageAvatarrWidth : 50}
+                  height={_imageAvatarrHeight ? _imageAvatarrHeight : 50}
+                  layout='responsive'
+                  objectFit='contain'
+                  quality={100}
+                />
+              </div>
+              <div className={styles['author__name']}>{author}</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+export default React.memo(SubBannerCarousel);
