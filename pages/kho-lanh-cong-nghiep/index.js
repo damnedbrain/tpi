9874dbb8738ab5eb061ccfdc4ae3e7ea8e850945
BinhDo <dt.binh@live.com>
2023-04-05 21:58:24 +0700

    Update banner component cho page Kho Lanh

--- pages/kho-lanh-cong-nghiep/index.js
+++ pages/kho-lanh-cong-nghiep/index.js
@@ -1,11 +1,41 @@
 import { Layout } from '@my-components';
 import { ColdStorage } from '@pages/public/logistic-hub/cold-storage';
+import { createClient } from 'contentful';
 
-const HomePage = () => {
+export async function getStaticProps({ locale }) {
+  try {
+    const client = createClient({
+      space: process.env.CONTENTFUL_SPACE_ID,
+      // environment: 'master', // defaults to 'master' if not set
+      accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+    });
+
+    const res = await client.getEntries({
+      content_type: 'toanPhatMarketNews',
+      locale: locale,
+    });
+
+    if (!res) {
+      return {
+        notFound: true,
+      };
+    }
+
+    return {
+      props: {
+        news: res.items,
+      },
+    };
+  } catch (error) {
+    // res.statusCode = 404;
+    return { props: {} };
+  }
+}
+
+export default function HomePage({news}) {
   return (
     <Layout>
-      <ColdStorage />
+      <ColdStorage news={news} />
     </Layout>
   );
 };
-export default HomePage;
--- src/pages/public/logistic-hub/cold-storage/ColdStorage.component.jsx
+++ src/pages/public/logistic-hub/cold-storage/ColdStorage.component.jsx
@@ -6,13 +6,15 @@ import { ColdStorageTopContent } from './cold-storage-top-content';
 import { ColdStorageListImage } from './cold-storage-list-image';
 import { NewsBlock } from 'components/own/news-block';
 import { breadcrumb } from '@constants/language-option';
+import { BannerPromo } from 'components/own/banner-promo';
 
-const ColdStorage = () => {
+function ColdStorage({news}) {
   return (
     <>
       <div className={styles['container']}>
         <Breadcrumb data={breadcrumb.coldStorage} />
-        <VideoTop />
+        <BannerPromo news={news} />
+        {/*<VideoTop />*/}
         <ColdStorageTopContent />
         <ColdStorageListImage />
          
