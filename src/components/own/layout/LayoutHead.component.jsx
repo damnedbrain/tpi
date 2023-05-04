b1d9289f8b6822356568592e5bba51e143bf4ecd
BinhDo <dt.binh@live.com>
2023-05-04 11:57:53 +0700

    Revert "Fix font 2"
    This reverts commit 12a62b80458d4ca83a933fde087a5a9c8f23f73d.

--- src/components/own/layout/LayoutHead.component.jsx
+++ src/components/own/layout/LayoutHead.component.jsx
@@ -13,9 +13,6 @@ const LayoutHead = ({ SEOInfo }) => (
     />
     <link rel='icon' href='/favicon.ico' />
     {/* <link href='/fonts/all.min.css' rel='stylesheet' /> */}
-    <link rel="preconnect" href="https://fonts.googleapis.com"/>
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>  
-    <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@100;300;400;500;700&display=swap" rel="stylesheet"></link>
 
   
 
