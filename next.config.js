efcf615e0e7915e38dbda9c162406fcdaaed14cb
BinhDo <dt.binh@live.com>
2023-05-04 11:12:21 +0700

    Fix Font

--- next.config.js
+++ next.config.js
@@ -21,10 +21,6 @@ module.exports = {
   compiler: {
     styledComponents: true,
   },
-  experimental: {
-    // Enables the styled-components SWC transform
-    styledComponents: true,
-  },
   eslint: {
     dirs: ['pages', 'src'], // Only run ESLint on the 'pages' and 'utils' directories during production builds (next build)
     ignoreDuringBuilds: true,
--- src/components/own/layout/LayoutHead.component.jsx
+++ src/components/own/layout/LayoutHead.component.jsx
@@ -14,17 +14,7 @@ const LayoutHead = ({ SEOInfo }) => (
     <link rel='icon' href='/favicon.ico' />
     {/* <link href='/fonts/all.min.css' rel='stylesheet' /> */}
 
-    <link rel='preconnect' href='https://fonts.googleapis.com' />
-    <link
-      rel='preconnect'
-      href='https://fonts.gstatic.com'
-      crossOrigin='true'
-    />
-
-    <link
-      href='https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@300;400;500;600;700&display=swap'
-      rel='stylesheet'
-    ></link>
+  
 
     <meta name='title' content={SEOInfo?.title || TITLE} />
     <meta name='description' content={SEOInfo?.desc || ''} />
