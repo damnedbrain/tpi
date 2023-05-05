0b59b5f5108ec5d70bce32f537a550ec57ba7a03
BinhDo <dt.binh@live.com>
2023-05-05 11:55:21 +0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -32,6 +32,17 @@ function MyApp({ Component, pageProps }) {
     };
   }, [setIsLoading]);
 
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = url => {
+      gtag.pageview(url)
+    }
+    router.events.on("routeChangeComplete", handleRouteChange)
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange)
+    }
+  }, [router.events])
+
   return <>
   {isLoading ? <ScreenLoading /> : 
     <>
@@ -39,7 +50,7 @@ function MyApp({ Component, pageProps }) {
 
         <Script
         strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
         />
         <Script
           id="gtag-init"
@@ -49,7 +60,7 @@ function MyApp({ Component, pageProps }) {
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
-              gtag('config', '${gtag.GA_TRACKING_ID}', {
+              gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
                 page_path: window.location.pathname,
               });
             `,
