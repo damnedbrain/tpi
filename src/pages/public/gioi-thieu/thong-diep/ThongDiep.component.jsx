6f55b0e01c34a9d871b7a073657616beb896d9ea
BinhDo <dt.binh@live.com>
2023-05-22 11:14:29 +0700

    TOANPHAT

--- src/pages/public/gioi-thieu/thong-diep/ThongDiep.component.jsx
+++ src/pages/public/gioi-thieu/thong-diep/ThongDiep.component.jsx
@@ -8,7 +8,7 @@ import about_us_img from '@assets/gioi-thieu/About-Us-Image-message.png';
 const title = `Thông điệp Ban Lãnh Đạo`;
 const desc = `Việt Nam có nhiều tiềm năng để phát triển Nông nghiệp, Đồng bằng sông Cửu Long là trung tâm lớn của ngành sản xuất Nông - Thủy sản, tuy nhiên những lợi thế đó vẫn chưa được tận dụng để có thể khẳng định thương hiệu và giá trị Nông - Thuỷ sản Quốc gia trên thị trường Quốc tế. 
 
-Toàn Phát Group ra đời với tầm nhìn trở thành một Trung tâm Logistics (Logistics HUB) cung cấp cả 03 dịch vụ: “Chiếu xạ - Lưu trữ - Vận chuyển” với công nghệ hiện đại, kỹ thuật tiên tiến và chi phí tối ưu nhất. Chúng tôi luôn cam kết đồng hành về giá, hỗ trợ các doanh nghiệp xuất khẩu Nông - Thủy sản có thêm lợi thế cạnh tranh trên thị trường Quốc tế.
+TOANPHAT GROUP ra đời với tầm nhìn trở thành một Trung tâm Logistics (Logistics HUB) cung cấp cả 03 dịch vụ: “Chiếu xạ - Lưu trữ - Vận chuyển” với công nghệ hiện đại, kỹ thuật tiên tiến và chi phí tối ưu nhất. Chúng tôi luôn cam kết đồng hành về giá, hỗ trợ các doanh nghiệp xuất khẩu Nông - Thủy sản có thêm lợi thế cạnh tranh trên thị trường Quốc tế.
 
 Từng mục tiêu nhỏ hướng về mục tiêu lớn, chúng tôi không chỉ nỗ lực đầu tư cả về trí tuệ con người, công nghệ hiện đại, trang thiết bị tiên tiến, mà còn quan tâm đến phản hồi của khách hàng để nâng cao chất lượng dịch vụ, tạo dựng uy tín thương hiệu và đưa TPG ngày càng vững mạnh.
 
