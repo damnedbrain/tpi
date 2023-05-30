541d0fe500ce11141b0e0ab51edea25685454a22
BinhDo <dt.binh@live.com>
2023-05-30 21:32:07 +0700

    Update LocationMap.component.jsx

--- src/pages/public/location/location-map/LocationMap.component.jsx
+++ src/pages/public/location/location-map/LocationMap.component.jsx
@@ -6,24 +6,6 @@ import { useObserverItem } from 'components/hook/useObserverItem';
 import { useChooseLanguage } from 'components/hook/useChooseLanguage';
 import { LocationMapContent } from '@constants/language-option';
 
-const videoLocationInfo =
-  'https://res.cloudinary.com/dy2gaer1o/video/upload/v1680506072/videos/location_map_video_a4mhx7.mp4';
-
-const opts = {
-    playerVars: {
-      // https://developers.google.com/youtube/player_parameters
-      autoplay: 1,
-      controls: 0,
-      showinfo: 0,
-      modestbranding: 1,
-      rel: 0,
-      enablejsapi: 1,
-      disablekb: 1,
-      mute: 1,
-      loop: 1,
-      playsinline: 1,
-    },
-};
 
 const LocationMap = () => {
   const refContent = useRef(null);
@@ -41,7 +23,7 @@ const LocationMap = () => {
           </div>
           <div className={styles['video']}>
           <video autoPlay muted loop className={styles['video']}>
-            <source src={videoLocationInfo} type='video/mp4' />
+            <source src="/location_map_video.mp4" type='video/mp4' />
           </video>
         </div>
           <div className={styles['desc-wrapper__desc']}>
