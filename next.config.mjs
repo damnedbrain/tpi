bf8d82c5e0da293166fecaa8f7f7389eab8e5619
BinhDo <35956710+damnedbrain@users.noreply.github.com>
2024-03-13 22:01:15 +0700

    1st

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    i18n: {
-        locales: ["en-US", "vi"],
-        defaultLocale: "vi",
-    },
+  reactStrictMode: true,
 };
 
 export default nextConfig;
