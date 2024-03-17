e8a2a79c9600e5abfb77742cf340e0f5b16805b9
BinhDo <35956710+damnedbrain@users.noreply.github.com>
2024-03-18 00:41:53 +0700

    update irr

--- src/pages/chieu-xa/index.js
+++ src/pages/chieu-xa/index.js
@@ -3,12 +3,18 @@ import { useRouter } from "next/router";
 import { foodIrradiationSystem, productOfIrradiation, subMethodIrradiation } from "@/constants/language-option";
 
 export default function ChieuXa() {
+
+    const router = useRouter();
+    const locale = router.locale;
+    const foodIrradiationSystemLocale = foodIrradiationSystem.find(item => item.locale === locale);
     return (
         <>
             <div className="flex flex-col md:flex-col justify-center items-center max-w-7xl h-auto m-auto mt-8">
                 <div className="flex flex-row items-start justify-end">
                     <h1 className="text-bold text-gray-200 text-6xl">1</h1>
-                    <h1>Chieu Xa</h1>
+                    <h1 className="text-bold text-gray-200 text-6xl">
+                        {foodIrradiationSystemLocale.title}
+                    </h1>
                 </div>
             </div>
 
