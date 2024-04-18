d5fba97da85e6ee46181c06207a5261fb7c4351c
damnedbrain <dt.binh@live.com>
2024-04-19 02:47:16 +0700

    more

--- src/pages/dang-nhap/index.js
+++ src/pages/dang-nhap/index.js
@@ -3,9 +3,9 @@ import ComingSoonBG from '@assets/dang-nhap/bg.png';
 
 export default function VanChuyen() {
     return (
-        <div className="flex flex-col md:flex-col justify-center items-center max-w-7xl h-auto m-auto mt-36">
+        <div className="flex flex-col md:flex-col justify-center items-center max-w-7xl h-auto m-auto mt-12">
             <h1 className="text-6xl font-bold text-green-800">COMING SOON</h1>
-            <Image src={ComingSoonBG} alt="coming-soon" className="w-full h-auto" />
+            <Image src={ComingSoonBG} alt="coming-soon" className="w-full h-auto mt-4" />
         </div>
     )
 }
