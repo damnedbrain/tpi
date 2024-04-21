1de6ffebd22af3aa285141f92ff8a0b906278373
damnedbrain <dt.binh@live.com>
2024-04-22 03:20:01 +0700

    video

--- src/components/content-ui/VideoComponent/index.js
+++ src/components/content-ui/VideoComponent/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const VideoComponent = ({ videoSrc, videoTitle, ...props }) => (
     <div className="video-container">
-      <video autoPlay loop muted>
+      <video autoPlay loop muted playsInline>
         <source src={videoSrc} type="video/mp4" />
         Your browser does not support the video tag.
       </video>
